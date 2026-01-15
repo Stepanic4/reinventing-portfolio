e75cfd4baa6a87874dda640bd7f41591f2dbca06
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-01-15 20:19:16 +0100

    everything is vorking,next  start work in dark them

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
         /* Добавляем flex flex-col и min-h-screen, чтобы всё приложение занимало минимум всю высоту экрана */
         <div className="flex flex-col min-h-screen bg-[#020617] text-slate-200 selection:bg-blue-500/30">
 
-            {/* Фоновые градиенты */}
-            <div className="fixed inset-0 overflow-hidden pointer-events-none">
-                <div
-                    className="absolute -top-[10%] -left-[10%] w-[40%] h-[40%] bg-blue-900/20 blur-[120px] rounded-full"></div>
-                <div
-                    className="absolute top-[20%] -right-[10%] w-[30%] h-[50%] bg-indigo-900/10 blur-[120px] rounded-full"></div>
-            </div>
-
             <Header currentPage={currentPage} setPage={setCurrentPage}/>
 
             {/* Добавляем flex-grow. Этот блок растянется и вытолкнет футер вниз */}
