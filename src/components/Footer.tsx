eb540e146cff52e8f08ca7c830402a4834b0fbbe
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-01-17 22:42:33 +0100

    change only link in my portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
             </a>
 
             {/* Центральный блок: Old portfolio */}
-            <a href="Old portfolio" target="_blank"
+            <a href="https://stepanic4.github.io/Portfolio" target="_blank"
                className="flex flex-col items-center gap-1 hover:text-blue-700 transition-all duration-300 group hover:-translate-y-1">
                 <svg className="h-5 w-5 group-hover:scale-110 transition-transform duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
