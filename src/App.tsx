a286208b0bfa1d47a5e2952ef717271141deab6c
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-01-25 20:52:50 +0100

    skeleton is done

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import {Header} from './components/Header';
 import {Hero} from './components/Hero';
 import {Footer} from "./components/Footer.tsx";
 import {Resume} from "./components/Resume.tsx";
+import {Works} from "./components/Works.tsx";
 
 function App() {
-       const [currentPage, setCurrentPage] = useState('main');
+    const [currentPage, setCurrentPage] = useState('main');
 
-     return (
-          <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
+    return (
+        <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
 
-              <div className="print:hidden">
-                 <Header currentPage={currentPage} setPage={setCurrentPage}/>
-             </div>
+            <div className="print:hidden">
+                <Header currentPage={currentPage} setPage={setCurrentPage}/>
+            </div>
 
-            {/* Добавляем flex-grow. Этот блок растянется и вытолкнет футер вниз */}
             <main className="relative pt-20 flex-grow">
                 {/* Главная страница */}
                 {currentPage === 'main' && (
@@ -23,23 +23,22 @@ function App() {
                     </div>
                 )}
 
-                {/* Заглушки для других страниц */}
                 {currentPage === 'works' && (
-                    <div className="flex justify-center pt-40">
-                        <h2 className="text-3xl font-bold">Мои работы (Coming Soon)</h2>
+                    <div className="flex justify-center">
+                        <Works/>
                     </div>
                 )}
 
                 {currentPage === 'resume' && (
                     <div className="flex justify-center pt-40">
-                        {currentPage === 'resume' && <Resume />}
+                        {currentPage === 'resume' && <Resume/>}
                     </div>
                 )}
             </main>
 
-             <div className="print:hidden">
-                 <Footer />
-             </div>
+            <div className="print:hidden">
+                <Footer/>
+            </div>
         </div>
     );
 }
