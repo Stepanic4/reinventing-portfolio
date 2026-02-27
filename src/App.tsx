49c95144cedec35c35169c7a261110629d241bc0
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-02-27 21:58:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ScrollToTop from "./hooks/ScrollToTop";
 function App() {
     const [currentPage, setCurrentPage] = useState('main');
 
-            return (
+             return (
         <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
              <ScrollToTop currentPage={currentPage} />
                 <div className="print:hidden">
