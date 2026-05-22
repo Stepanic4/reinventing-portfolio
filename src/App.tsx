bf4de0e07a456a7feaf0cd053bcdd498aba61db7
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-05-22 15:03:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState("main");
 
   return (
-    <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
+     <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
       <ScrollToTop currentPage={currentPage} />
       <div className="print:hidden">
         <Header currentPage={currentPage} setPage={setCurrentPage} />
