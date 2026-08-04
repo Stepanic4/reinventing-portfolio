d13674c6500a79a5415758aa5f0637532a42096e
Ivan <Zolotukhin.ivan.1@gmail.com>
2026-08-04 20:45:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
                     <div className="flex flex-col min-h-screen transition-colors duration-500 dark:bg-[#020617]  selection:bg-blue-500/30">
        <ScrollToTop currentPage={currentPage} />
-            <div className="print:hidden">
+             <div className="print:hidden">
         <Header currentPage={currentPage} setPage={setCurrentPage} />
       </div>
 
